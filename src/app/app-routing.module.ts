ca7ed09dda094f586502bb2e6be727f50b886315
Chenge Chakoma <Chenge@MacBook-Pro.fritz.box>
2022-09-07 03:55:39 +0200

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: 'books/:id',
     loadChildren: () => import('./book-details/book-details.module').then( m => m.BookDetailsPageModule)
   },
